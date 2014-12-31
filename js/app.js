e41cd4addc3d8efbaea6ec16c4a4838645101fe3
cespirit <ckespir@gmail.com>
2014-12-31 10:07:32 -0800

    Adjust layout and style

--- js/app.js
+++ js/app.js
@@ -4,37 +4,40 @@ $(document).ready(function(){
 		event.preventDefault();
 		var searchTerm = $("#query").val();
 		getRequest(searchTerm);
-		
+		$("#query").val("");
 	});
 
 	function getRequest(searchTerm) {
 		var params = {
 			part: "snippet",
 			key: "AIzaSyCrPY0jdtrE5sYaxdin3-Km3h9za8MjibE",
-			maxResults: 16,
+			maxResults: 20,
 			order: "relevance",
-			q: searchTerm
+			q: searchTerm,
+			type: "video"
 		}
 		url = "https://www.googleapis.com/youtube/v3/search";
 
 		$.getJSON(url, params, function(data) {
+			$(".search-details").html("<p>Results for <strong>&#34;" + searchTerm + "&#34;</strong></p>" );
 			showResults(data.items);
 			console.log(data);
 		});
 	}
 
 	function showResults(videos) {
-	    /* Video image thumbnail alt tag */
-
 	    var html ="";
 	    var videoImgURL;
 	    var videoTitle;
+	    
 	    $.each(videos, function(index, value) {
 	    	var videoURL = "http://youtube.com/watch?v=";
 	    	videoURL += value["id"]["videoId"];
 	    	videoTitle = value["snippet"]["title"];
 	    	videoImgURL = value["snippet"]["thumbnails"]["medium"]["url"];
-	    	html += "<a href='" + videoURL + "'>" + "<img src='" + videoImgURL + "' alt='" + videoTitle + " video" + "'></a>";
+	    	html += "<a href='" + videoURL + "'>" + "<img src='" + videoImgURL +
+	    		"' title='" + videoTitle + 
+	    		"' alt='" + videoTitle + " video" + "'></a>"; 
 	    });
 
 	    $("#search-results").html(html);
